4eace4dc5ae6e086d4433c8588bbf1d4666940aa
Eccentric-art <pmb1986@yahoo.com>
2024-09-24 20:30:57 +0100

    first commit

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Project from "../Components/Project";
+
+const Home = () => {
+  return (
+    <div>
+      <Project />
+    </div>
+  );
+};
+
+export default Home;
